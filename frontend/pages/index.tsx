c2d699de4cdee81f0c721cf1a8ec1ca6e97512cf
Ben Sonley <lbsonley@gmail.com>
2022-03-31 10:16:43 +0200

    feat(frontend): add landing page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,14 @@
 /* eslint-disable import/no-relative-packages */
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import styled from 'styled-components';
+import StyledButton from '../components/primitives/button/button';
+
+const Slogan = styled.p`
+  font-family: 'Montserrat', sans-serif;
+  font-size: 100px;
+  margin: 40px 0;
+`;
 
 const Home: NextPage = () => {
   return (
@@ -9,7 +17,26 @@ const Home: NextPage = () => {
         <title>Home | Reformat App</title>
         <meta name="description" content="Reformat App Home" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
+      <div>
+        <Slogan>
+          LEBST
+          <br />
+          DU
+          <br />
+          DEIN
+          <br />
+          TALENT
+          <br />?
+        </Slogan>
+        <StyledButton>Sign me up!</StyledButton>
+      </div>
     </div>
   );
 };
